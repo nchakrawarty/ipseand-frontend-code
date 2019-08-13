4d1deb919708c81814b7380f1e0a6af9a6b0c05b
nchakrawarty <nchakrawarty@gmail.com>
2019-08-13 16:38:39 +0530

    added category

---
+++ projects/user-app/src/app/shared/sdk/models/Category.ts
@@ -0,0 +1,82 @@
+/* tslint:disable */
+
+declare var Object: any;
+export interface CategoryInterface {
+  "name": string;
+  "productId"?: Array<any>;
+  "subCategoryId"?: Array<any>;
+  "description"?: string;
+  "image"?: string;
+  "id"?: any;
+}
+
+export class Category implements CategoryInterface {
+  "name": string;
+  "productId": Array<any>;
+  "subCategoryId": Array<any>;
+  "description": string;
+  "image": string;
+  "id": any;
+  constructor(data?: CategoryInterface) {
+    Object.assign(this, data);
+  }
+  /**
+   * The name of the model represented by this $resource,
+   * i.e. `Category`.
+   */
+  public static getModelName() {
+    return "Category";
+  }
+  /**
+  * @method factory
+  * @author Jonathan Casarrubias
+  * @license MIT
+  * This method creates an instance of Category for dynamic purposes.
+  **/
+  public static factory(data: CategoryInterface): Category{
+    return new Category(data);
+  }
+  /**
+  * @method getModelDefinition
+  * @author Julien Ledun
+  * @license MIT
+  * This method returns an object that represents some of the model
+  * definitions.
+  **/
+  public static getModelDefinition() {
+    return {
+      name: 'Category',
+      plural: 'categories',
+      path: 'categories',
+      idName: 'id',
+      properties: {
+        "name": {
+          name: 'name',
+          type: 'string'
+        },
+        "productId": {
+          name: 'productId',
+          type: 'Array&lt;any&gt;'
+        },
+        "subCategoryId": {
+          name: 'subCategoryId',
+          type: 'Array&lt;any&gt;'
+        },
+        "description": {
+          name: 'description',
+          type: 'string'
+        },
+        "image": {
+          name: 'image',
+          type: 'string'
+        },
+        "id": {
+          name: 'id',
+          type: 'any'
+        },
+      },
+      relations: {
+      }
+    }
+  }
+}
